f4e115e69f67e146220ec9557e3b6b72435e40d8
Fanny Reslow <reslowf@gmail.com>
2022-05-20 22:07:42 +0200

    creating offline mode

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-restricted-globals */
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll(["offline.html", "app.css", "index.css"]);
+    })
+  );
+  self.skipWaiting();
+  console.log("sw installed", new Date().toLocaleTimeString());
+});
+
+self.addEventListener("activate", () => {
+  self.skipWaiting();
+  console.log("sw activated", new Date().toLocaleTimeString());
+});
+console.log("då");
+// kolla om jag är online eller offline
+
+self.addEventListener("fetch", async (event) => {
+  console.log(event);
+
+  if (!navigator.onLine) {
+    console.log("offline");
+
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        console.log("response", response);
+        if (response) {
+          return response;
+        } else {
+          return caches.match(new Request("offline.html"));
+        }
+      })
+    );
+  } else {
+    console.log("online");
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// regga sw
+
+async function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("sw.js");
+    } catch (error) {
+      console.log("så man kan göra , men fel fel fel", error);
+    }
+  }
+}
+
+registerServiceWorker();

b9544ce0bc716e883b828ba9e4c1fd04ad35e156
Fanny Reslow <reslowf@gmail.com>
2022-05-22 22:02:49 +0200

    save to localstorage and display from localstorage

---
+++ src/components/ImagesElement.js
@@ -0,0 +1,3 @@
+export default function ImageElement({ image }) {
+  return <img src={image} alt="a" />;
+}
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,8 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import ImageElement from "./ImagesElement";
 
 export default function Photo() {
   const videoReference = useRef();
   const canvasRef = useRef();
+  const [images, setImges] = useState([]);
 
   async function handleStartCamera() {
     if ("mediaDevices" in navigator) {
@@ -13,16 +15,33 @@ export default function Photo() {
   }
 
   function handleTakeAPhoto() {
-    console.log("snap");
-    const width = 400;
-    const height = 600;
     let video = videoReference.current;
+    console.log(video);
 
     let ctx = canvasRef.current.getContext("2d");
+    ctx.drawImage(
+      video,
+      0,
+      0,
+      canvasRef.current.height,
+      canvasRef.current.width
+    );
     console.log(ctx);
+    const imageData = canvasRef.current?.toDataURL("image/png");
 
-    ctx.drawImage(video, 0, 0, height, width);
+    console.log(imageData);
+    images.push({
+      id: images.length,
+      image: imageData,
+    });
+
+    setImges(images);
+
+    localStorage.setItem("cameraApp", JSON.stringify(images));
   }
+
+  let imageCollection = JSON.parse(localStorage.getItem("cameraApp"));
+  console.log(imageCollection);
 
   return (
     <section>
@@ -33,10 +52,14 @@ export default function Photo() {
         Ta kort
       </button>
       <video src="" id="camera" autoPlay ref={videoReference}></video>
-      {<canvas id="canvas" ref={canvasRef}></canvas>}
+      {<canvas id="canvas" ref={canvasRef} height="480" width="640"></canvas>}
       <section>
         <h3>Galleri</h3>
-        <section id="gallery"></section>
+        <section id="gallery">
+          {imageCollection?.map((image, index) => {
+            return <ImageElement image={image.image} key={index} />;
+          })}
+        </section>
       </section>
     </section>
   );

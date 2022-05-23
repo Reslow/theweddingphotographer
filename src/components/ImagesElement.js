5b14eef1096e5cdfab111167ab32462a74883051
Fanny Reslow <reslowf@gmail.com>
2022-05-23 15:13:01 +0200

    adding images and deleting them i gallery

--- src/components/ImagesElement.js
+++ src/components/ImagesElement.js
@@ -1,3 +1,21 @@
-export default function ImageElement({ image }) {
-  return <img src={image} alt="a" />;
+export default function ImageElement({ image, images, setImages }) {
+  console.log(images);
+
+  function handleRemoveImage() {
+    console.log("click");
+    console.log(images);
+    let newImageArr = images.filter((img) => img.image !== image);
+    console.log(typeof newImageArr.length);
+    console.log(newImageArr);
+    setImages(newImageArr);
+    console.log(images);
+    localStorage.setItem("cameraApp", JSON.stringify(newImageArr));
+  }
+
+  return (
+    <section>
+      <img src={image} alt="a" />
+      <button onClick={handleRemoveImage}>remove</button>
+    </section>
+  );
 }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,7 +4,15 @@ import ImageElement from "./ImagesElement";
 export default function Photo() {
   const videoReference = useRef();
   const canvasRef = useRef();
-  const [images, setImges] = useState([]);
+
+  let itemsInLocalstorage = JSON.parse(localStorage.getItem("cameraApp"));
+  console.log(itemsInLocalstorage);
+
+  const [images, setImages] = useState(
+    itemsInLocalstorage?.length > 0 ? itemsInLocalstorage : []
+  );
+
+  const [takenPhoto, setTakenPhoto] = useState(false);
 
   async function handleStartCamera() {
     if ("mediaDevices" in navigator) {
@@ -16,8 +24,6 @@ export default function Photo() {
 
   function handleTakeAPhoto() {
     let video = videoReference.current;
-    console.log(video);
-
     let ctx = canvasRef.current.getContext("2d");
     ctx.drawImage(
       video,
@@ -26,23 +32,24 @@ export default function Photo() {
       canvasRef.current.height,
       canvasRef.current.width
     );
-    console.log(ctx);
+
     const imageData = canvasRef.current?.toDataURL("image/png");
 
-    console.log(imageData);
     images.push({
       id: images.length,
       image: imageData,
     });
+    setImages(images);
 
-    setImges(images);
-
-    localStorage.setItem("cameraApp", JSON.stringify(images));
+    setTakenPhoto(true);
   }
 
-  let imageCollection = JSON.parse(localStorage.getItem("cameraApp"));
-  console.log(imageCollection);
+  function handleSaveImage() {
+    localStorage.setItem("cameraApp", JSON.stringify(images));
+    setImages(JSON.parse(localStorage.getItem("cameraApp")));
+  }
 
+  console.log(images);
   return (
     <section>
       <button id="start-camera" onClick={handleStartCamera}>
@@ -52,13 +59,23 @@ export default function Photo() {
         Ta kort
       </button>
       <video src="" id="camera" autoPlay ref={videoReference}></video>
-      {<canvas id="canvas" ref={canvasRef} height="480" width="640"></canvas>}
+
+      <canvas id="canvas" ref={canvasRef} height="480" width="640"></canvas>
+      {takenPhoto && <button onClick={handleSaveImage}>Save</button>}
       <section>
         <h3>Galleri</h3>
         <section id="gallery">
-          {imageCollection?.map((image, index) => {
-            return <ImageElement image={image.image} key={index} />;
-          })}
+          {images.length > 0 &&
+            images.map((image, index) => {
+              return (
+                <ImageElement
+                  image={image.image}
+                  images={images}
+                  key={index}
+                  setImages={setImages}
+                />
+              );
+            })}
         </section>
       </section>
     </section>
